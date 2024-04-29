ba586255cdf9df5d50f4ff763c31096f8d0fde08
Virtualvoyage37 <virtualvoyage37@gmail.com>
2024-04-29 10:45:13 +0530

    Counter

---
+++ 03CounterProps/src/Deepak.jsx
@@ -0,0 +1,17 @@
+function Deepak2() {
+  return (
+    <div>
+      <div>Heloo Deepak</div>
+    </div>
+  );
+}
+function Demo2() {
+    const data = "i am"
+  return (
+    <div>
+      <div>{data} Demo2</div>
+    </div>
+  );
+}
+
+export {Deepak2,Demo2};
