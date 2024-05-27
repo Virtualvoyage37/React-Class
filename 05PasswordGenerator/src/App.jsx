49104b6e6ec8f0fe4faf3dabeb2683606d2cfcec
Virtualvoyage37 <virtualvoyage37@gmail.com>
2024-05-27 09:11:32 +0530

    UseRef-Hook

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { useState } from "react";
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
   const [Password, setPassword] = useState("");
   const [numberAllow, setNumberAllow] = useState(false);
   const [charAllow, setCharAllow] = useState(false);
+
+  const CopyInput = useRef();
 
   const PasswordGenerator = useCallback(() => {
     let pass = "";
@@ -27,14 +29,19 @@ function App() {
 
   const CopyPassword = () => {
     window.navigator.clipboard.writeText(Password);
+
+    CopyInput.current.focus();
+    CopyInput.current.select();
+    // CopyInput.current.style.backgroundColor ="red";
+
   };
 
   return (
-    <>
-      <div className="flex justify-center items-center bg-gray-600 text-white max-w-[850px] m-auto rounded-xl max-h-[500px] h-full p-5 flex-col gap-5">
+    <div className="flex justify-center items-center h-screen   ">
+      <div className="flex w-full justify-center items-center bg-gray-600 text-white max-w-[850px] m-auto rounded-xl max-h-[500px] h-full p-5 flex-col gap-5 ">
         <h1 className="text-3xl font-bold ">Password Generator</h1>
         <div>
-          <input type="text" className="input" value={Password} readOnly />
+          <input ref={CopyInput} type="text" className={`input `} value={Password} readOnly />
           <button className="btn h-[40px]" onClick={() => CopyPassword()}>
             Copy
           </button>
@@ -61,7 +68,7 @@ function App() {
           />
           <label className="">Number</label>
         </div>
-        <div className="flex-center gap-4">
+        <div className="flex-center items-start gap-4">
           <input
             type="checkbox"
             value={charAllow}
@@ -71,7 +78,7 @@ function App() {
           <label className="">Char</label>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
